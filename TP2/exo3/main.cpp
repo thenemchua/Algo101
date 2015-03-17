6e69fda7b873767b8f4d37167d727423437394dd
Minijackson <minijackson@riseup.net>
2015-03-17 18:16:43 +0100

    Adding random vector initialization

---
+++ TP2/exo3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+std::ostream& operator<<(std::ostream& os, std::vector<int> const& tab) {
+	for(auto const& el : tab) {
+		os << el << " ";
+	}
+	return os;
+}
+
+int main (int argc, char const* argv[]) {
+
+	if(argc == 1) {
+		std::cout << "Usage: " << argv[0] << " n" << std::endl;
+		return 64;
+	}
+
+	std::default_random_engine random_engine((std::random_device()()));
+	std::uniform_int_distribution<int> distribution(0, 100);
+
+	distribution(random_engine);
+
+	std::vector<int> tab(std::stoi(argv[1]));
+
+	for(int& el : tab) {
+		el = distribution(random_engine);
+	}
+
+	std::cout << tab << std::endl;
+
+
+	return 0;
+}
