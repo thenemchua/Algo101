9478bdf92a860ef69144fbb9cb4bcc92d8be8b88
mininao <maxence_aici@yahoo.fr>
2015-03-30 14:40:17 +0200

    Commenting exo1

--- TP2/exo1/question2.cpp
+++ TP2/exo1/question2.cpp
@@ -5,7 +5,24 @@ using boost::numeric::ublas::identity_matrix;
 using boost::numeric::ublas::prod;
 
 namespace question2 {
-
+/**
+ *	Cette fonction permet d'élever une matrice A à la puissance B
+ *
+ *	Invariant : I(M,R,c) 
+ *	⇒ A^b = M * A^c * R
+ *
+ *	Initialisation :
+ *	M = R = Identité(n) (n est la taille de A)
+ *	c = b
+ *	
+ *
+ *	Arrêt :
+ *	c == 1
+ *
+ *	Progression :
+ *	I(M,c) ∧ c pair   ⇒ I(M*M,c/2) (1)
+ *	I(M,c) ∧ c impair ⇒ I(M*M, (c-1)/2 , R * M) (2)
+ */
 	matrix<int> operator*(matrix<int> const& A, matrix<int> const& B) {
 		return static_cast<matrix<int>>(prod(A, B));
 	}
@@ -32,7 +49,7 @@ namespace question2 {
 		std::cout << rest << std::endl;
 
 		while(exp > 1) {
-			if(exp % 2 == 1) {
+			if(exp % 2 == 1) { // Si la puissance est impaire , on stocke le reste pour l'ajouter à la fin, Cas (2)
 				rest *= result;
 				--exp;
 			}
