53ae380658ad14a2dd5d6fa081dc4be39b8f2a62
mininao <maxence_aici@yahoo.fr>
2015-03-30 14:55:12 +0200

    toto

--- TP2/exo1/question3.cpp
+++ TP2/exo1/question3.cpp
@@ -6,21 +6,29 @@ using boost::numeric::ublas::matrix;
 using boost::numeric::ublas::prod;
 
 namespace question3 {
-
+	/**
+	 *	Cette fonction permet de trouver le nombre de fibonacci d'indice n, en un temps Θ(log n)
+	 */
 	int fibonacci(int n) {
+
+		// Cas pathologiques
 		if(n == 0) {
 			return 0;
 		} else if(n == 1) {
 			return 1;
 		}
 
+
 		matrix<int> mult(2,2);
+		// i = 1
 		mult(0,0) = 1;
 		mult(0,1) = 1;
+		// i = 0
 		mult(1,0) = 1;
 		mult(1,1) = 0;
 
-		mult   = question2::pow(mult, n-1);
+		// i ++
+		mult = question2::pow(mult, n-1);
 
 		return mult(0,0);
 	}
