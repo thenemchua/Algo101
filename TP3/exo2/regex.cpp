1b2f7eded21f6fc0e2ab41c52d33485f44b41551
Minijackson <minijackson@riseup.net>
2015-04-10 15:12:24 +0200

    Adding working exo2 (needs further testing)

--- TP3/exo2/regex.cpp
+++ TP3/exo2/regex.cpp
@@ -1,19 +1,66 @@
 #include "regex.hpp"
 
 bool match(std::string base, std::string pattern) {
+	return match(base, base.begin(), base.end(), pattern, pattern.begin(), pattern.end());
+}
+
+bool match(std::string const& base,
+           std::string::iterator baseBegin,    std::string::iterator baseEnd,
+		   std::string const& pattern,
+		   std::string::iterator patternBegin, std::string::iterator patternEnd) {
 
 	if(pattern.empty())
 		return base.empty();
 
 	if(base.empty())
-		return std::all_of(pattern.begin(), pattern.end(), [](char c){return c == '*';});
+		return std::all_of(patternBegin, patternEnd, [](char c){return c == '*';});
 
 	std::string::iterator firstSpecialCharacter(
-			std::find_if(pattern.begin(), pattern.end(), [](char c){return (c == '*') || (c == '%');})
+			std::find_if(patternBegin, patternEnd, [](char c){return (c == '*') || (c == '%');})
 		);
 
-	if(std::equal(pattern.begin(), firstSpecialCharacter, base.begin())) {
+	if(std::equal(patternBegin, firstSpecialCharacter, baseBegin)) {
 		// Now the fun begins
+		std::string::iterator baseNewPosition(baseBegin + std::distance(patternBegin, firstSpecialCharacter));
+
+		// No special characters
+		if(firstSpecialCharacter == patternEnd) {
+
+			if(baseNewPosition == baseEnd)
+				return true;
+			else
+				return false;
+
+		} else if(*firstSpecialCharacter == '%') {
+
+			// No room for the last '%'
+			if(baseNewPosition == baseEnd)
+				return false;
+			else
+				return match(base,    baseNewPosition + 1,       baseEnd,
+				             pattern, firstSpecialCharacter + 1, patternEnd);
+
+		// We have a '*' here!
+		} else {
+			// Go at the end of the '*' streak
+			std::string::iterator endOfStarStreak(
+				std::find_if(firstSpecialCharacter, patternEnd, [](char c){return c != '*';})
+			);
+
+			if(endOfStarStreak == patternEnd) {
+				return true;
+			} else {
+				char const& requiredCharacter(*endOfStarStreak);
+				baseNewPosition = std::find(baseNewPosition, baseEnd, requiredCharacter);
+
+				if(baseNewPosition == baseEnd)
+					return false;
+				else
+					return match(base,    baseNewPosition + 1, baseEnd,
+					             pattern, endOfStarStreak + 1, patternEnd);
+
+			}
+		}
 	} else {
 		return false;
 	}
--- TP3/exo2/regex.hpp
+++ TP3/exo2/regex.hpp
@@ -2,3 +2,7 @@
 #include <algorithm>
 
 bool match(std::string base, std::string pattern);
+bool match(std::string const& base,
+           std::string::iterator baseBegin,    std::string::iterator baseEnd,
+		   std::string const& pattern,
+		   std::string::iterator patternBegin, std::string::iterator patternEnd);
