a8eac40b0cb333cedb08ff521d9a77a991823107
Maxence Aïci <mininao@users.noreply.github.com>
2015-05-06 09:09:14 +0200

    Commenting Exercise 3

--- TP3/exo3/main.cpp
+++ TP3/exo3/main.cpp
@@ -7,10 +7,8 @@ typedef std::chrono::high_resolution_clock high_functioning_clock;
 typedef std::chrono::time_point<high_functioning_clock> timer;
 typedef std::chrono::duration<double, std::milli> duration;
 
-int fibonacci(int index) {
-	if(index < 0)
-		throw std::invalid_argument("The index for the Fibonacci sequence must be positive");
-	else if(index == 0)
+int fibonacci(unsigned int index) {
+	if(index == 0)
 		return 0;
 	else if(index <= 2)
 		return 1;
@@ -22,13 +20,13 @@ int main() {
 
 	std::ofstream fibonacciCSV("fibonacci.csv");
 
-	for(int i = 0 ; i <= 40 ; ++i) {
+	for(int i = 0 ; i <= 40 ; ++i) { // Do the computation for the first 40 elements
 		timer start = high_functioning_clock::now();
 
-		std::cout << "F_" << i << " = " << fibonacci(i) << std::endl;
+		std::cout << "F_" << i << " = " << fibonacci(i) << std::endl; // Print it to command prompt
 
-		duration result = high_functioning_clock::now() - start;
-		fibonacciCSV << i << ", "<< result.count() << std::endl;
+		duration result = high_functioning_clock::now() - start; 
+		fibonacciCSV << i << ", "<< result.count() << std::endl; // Print the time it took to a csv, see ../Fibonnacci.R for plotting
 	}
 	
 	return 0;
