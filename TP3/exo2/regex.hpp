26227159424d6f42602e8b6104085d51bd6922c1
Minijackson <minijackson@riseup.net>
2015-05-06 09:57:07 +0200

    Changing regex algorithm

--- TP3/exo2/regex.hpp
+++ TP3/exo2/regex.hpp
@@ -2,7 +2,5 @@
 #include <algorithm>
 
 bool match(std::string base, std::string pattern);
-bool match(std::string const& base,
-           std::string::iterator baseBegin,    std::string::iterator baseEnd,
-		   std::string const& pattern,
+bool match(std::string::iterator baseBegin,    std::string::iterator baseEnd,
 		   std::string::iterator patternBegin, std::string::iterator patternEnd);
