4413b63f2b0ab3b82bb5d37757c14410e2d2a460
Maxence Aïci <mininao@users.noreply.github.com>
2015-05-06 10:20:55 +0200

    Commenting Exercise 2

--- TP3/exo2/regex.cpp
+++ TP3/exo2/regex.cpp
@@ -1,27 +1,27 @@
 #include "regex.hpp"
 
 bool match(std::string base, std::string pattern) {
-	return match(base.begin(), base.end(), pattern.begin(), pattern.end());
+	return match(base.begin(), base.end(), pattern.begin(), pattern.end()); // We use iterators to traverse the arrays
 }
 
 bool match(std::string::iterator baseBegin,    std::string::iterator baseEnd,
 		   std::string::iterator patternBegin, std::string::iterator patternEnd) {
 
 	if(patternBegin == patternEnd)
-		return baseBegin == baseEnd;
+		return baseBegin == baseEnd; // If the pattern is empty, return true if the base is too
 
-	if(baseBegin == baseEnd)
+	if(baseBegin == baseEnd) // If the base is empty, return true if the pattern is empty or contains only "*"
 		return std::all_of(patternBegin, patternEnd, [](char c){return c == '*';});
 
-	if(*patternBegin != '*' && *patternBegin != '%') {
-		return (*patternBegin == *baseBegin)
-			&& match(baseBegin + 1, baseEnd, patternBegin + 1, patternEnd);
+	if(*patternBegin != '*' && *patternBegin != '%') { // If it's not a special character
+		return (*patternBegin == *baseBegin) // Return true if the character is identical 
+			&& match(baseBegin + 1, baseEnd, patternBegin + 1, patternEnd); // And if the rest of the pattern matches
 	} else if(*patternBegin == '*') {
-		return match(baseBegin,     baseEnd, patternBegin + 1, patternEnd)
-			|| match(baseBegin + 1, baseEnd, patternBegin,     patternEnd);
+		return match(baseBegin,     baseEnd, patternBegin + 1, patternEnd) // The "*" matches no character
+			|| match(baseBegin + 1, baseEnd, patternBegin,     patternEnd); // The "*" matches the next character
 	// We have a '%' here !
 	} else {
 		return match(baseBegin + 1,    baseEnd,
-		             patternBegin + 1, patternEnd);
+		             patternBegin + 1, patternEnd); // We skip to the next char
 	}
 }
