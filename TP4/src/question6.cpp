bed28ef1ddcebb4a00c377449421b5a2fe12c9b4
mininao <maxence_aici@yahoo.fr>
2015-05-29 16:19:35 +0200

    Adding comments to TP4/exo6

--- TP4/src/question6.cpp
+++ TP4/src/question6.cpp
@@ -4,32 +4,35 @@ using boost::numeric::ublas::matrix;
 
 namespace question6 {
 
-	std::vector<int> computeSpots(matrix<int> dynaMat) {
+	std::vector<int> computeSpots(matrix<int> dynaMat) { // On pacours la matrice à l'envers pour retrouver les spots utilisés
 		std::vector<int> spots;
 		int row(dynaMat.size1() - 1),
 		    col(dynaMat.size2() - 1);
 
 		bool goUp = true;
 
+		// goUp => On monte dans la matrice (Cas où le spot actuel n'est pas utilisé)
+
 		while(row > 0) {
 			if(col == 0) {
-				break;
+				break; // On s'arrête si on atteint la première colonne
 			}
 
 			int nextRow = (goUp) ? row-1 : row,
 			    nextCol = (!goUp)? col-1 : col;
 
-			if(dynaMat(row, col) > dynaMat(nextRow, nextCol)) {
+			if(dynaMat(row, col) > dynaMat(nextRow, nextCol)) { // Si la cellule actuelle est supérieure à la suivante, cela signifie qu'on change de cas
 				if(goUp) {
-					spots.push_back(row - 1);
+					spots.push_back(row - 1); // On ce le spot actuelomptabilis
 				}
 
-				// Invert direction and re-establish the next cell
+				// On inverse la direction et on met la bonne cellule
 				goUp = not goUp;
 				nextRow = (goUp) ? row-1 : row;
 				nextCol = (!goUp)? col-1 : col;
 			}
-
+			
+			//Case suivante 
 			row = nextRow;
 			col = nextCol;
 		}
