adce7d0b47fab2996c72f9393a27b527ae766cfe
mininao <maxence_aici@yahoo.fr>
2015-06-10 09:52:17 +0200

    Commenting main

---
+++ TP5/makeGraph.sh
@@ -0,0 +1,1 @@
+./main.bin | dot -Tsvg -O
--- TP5/src/main.cpp
+++ TP5/src/main.cpp
@@ -45,38 +45,45 @@ int main() {
 
 	// =============================== Smallest Dates ==========================
 
-	std::vector<int> smallestDates(14),
-	                 origins(14);
-	std::fill(smallestDates.begin(), smallestDates.end(), std::numeric_limits<int>::max());
+	std::vector<int> smallestDates(14), // Contient pour tout noeud i (de 0 à 13) la durée du chemin le plus court menant à lui
+	                 origins(14); // Contient pour tout noeud i (de 0 à 13) le noeud parent dans le chemin le plus court menant à lui. Kamoulox.
+
+	// Remplissage des noeuds parents avec -1	
 	std::fill(origins.begin(), origins.end(), -1);
 
 	smallestDates[0] = 0;
 
-	typedef std::tuple<std::vector<int>&, std::vector<int>&> Tabs;
+	typedef std::tuple<std::vector<int>&, std::vector<int>&> Tabs; // Un Tabs contient le couple (smallestDates,origins)
 
+	// calcSmallestDates est une fonction qui renvoie un Tabs et prend en paramètre (Vertex const& v, Edge const& e, Tabs& tabs)
+	// Elle prend en paramètre un noeud, l'arc suivant et l'état actuel des chemins les plus courts (Sous la forme d'un Tabs)
+	// Elle retourne le couple (smallestDates,origins) contenant le chemin le plus court menant au noeud suivant le noeud passé en paramètre
+	// ,après analyse du chemin incluant le noeud passé en paramètre
 	std::function<Tabs& (Vertex const&, Edge const&, Tabs&)> calcSmallestDates = [] (Vertex const& v, Edge const& e, Tabs& tabs) -> Tabs& {
 
 		std::vector<int> smallestDates(14), origins(14);
-		std::tie(smallestDates, origins) = tabs;
+		std::tie(smallestDates, origins) = tabs; //
 
-		int vertexName = v.getValue(),
-		    nextVertexName = e.getDestination().getValue(),
-			contestantValue = smallestDates[vertexName] + e.getWeight();
+		int vertexName = v.getValue(), // Numéro du noeud analysé
+		    nextVertexName = e.getDestination().getValue(), // Numéro du noeud suivant
+			contestantValue = smallestDates[vertexName] + e.getWeight(); // Valeur à comparer avec le meilleur poids actuel
 
 		if(origins[nextVertexName] == -1
-				|| smallestDates[nextVertexName] > contestantValue) {
+				|| smallestDates[nextVertexName] > contestantValue) { // Si il n'y a pas encore de plus court chemin OU si le nouveau chemin est meilleur
+			// On définit le plus court chemin comme étant le nouveau
 			smallestDates[nextVertexName] = contestantValue;
 			origins[nextVertexName] = vertexName;
 		}
 
-		tabs = std::tie(smallestDates, origins);
+		tabs = std::tie(smallestDates, origins); // On créé le nouveau tabs
 		return tabs;
 	};
 
 	Tabs tabs = std::tie(smallestDates, origins);
-	tabs = fold(g, tabs, calcSmallestDates);
+	tabs = fold(g, tabs, calcSmallestDates);// On applique calcSmallestDates à tous les éléments.
 	std::tie(smallestDates, origins) = tabs;
 
+	// Impression du graphe (parcours à l'envers, depuis l'origine)
 	std::cout << std::endl
 	          << "digraph G {" << std::endl;
 
@@ -100,41 +107,48 @@ int main() {
 	}
 
 	std::cout << "}" << std::endl;
+	// Fin de l'impression du graphe
 
 	// =============================== Highest Dates ==========================
 
 	std::vector<int> highestDates(14),
 	                 maxOrigins(14);
-	std::fill(highestDates.begin(), highestDates.end(), std::numeric_limits<int>::min());
+	// Remplissage des noeuds parents avec -1	
 	std::fill(maxOrigins.begin(), maxOrigins.end(), -1);
 
 	highestDates[0] = 0;
 
-	typedef std::tuple<std::vector<int>&, std::vector<int>&> Tabs;
+	typedef std::tuple<std::vector<int>&, std::vector<int>&> Tabs; // Un Tabs contient le couple (smallestDates,origins)
 
+	// calcHighestDates est une fonction qui renvoie un Tabs et prend en paramètre (Vertex const& v, Edge const& e, Tabs& tabs)
+	//Elle prend en paramètre un noeud, l'arc suivant et l'état actuel des chemins les plus longs (Sous la forme d'un Tabs)
+	// Elle retourne le couple (highestDates,origins) contenant le chemin le plus long menant au noeud suivant le noeud passé en paramètre
+	// ,après analyse du chemin incluant le noeud passé en paramètre
 	std::function<Tabs& (Vertex const&, Edge const&, Tabs&)> calcHighestDates = [] (Vertex const& v, Edge const& e, Tabs& tabs) -> Tabs& {
 
 		std::vector<int> highestDates(14), maxOrigins(14);
 		std::tie(highestDates, maxOrigins) = tabs;
 
-		int vertexName = v.getValue(),
-		    nextVertexName = e.getDestination().getValue(),
-			contestantValue = highestDates[vertexName] + e.getWeight();
+		int vertexName = v.getValue(), // Numéro du noeud analysé
+		    nextVertexName = e.getDestination().getValue(),  // Numéro du noeud suivant
+			contestantValue = highestDates[vertexName] + e.getWeight(); // Valeur à comparer avec le meilleur poids actuel
 
 		if(maxOrigins[nextVertexName] == -1
-				|| highestDates[nextVertexName] < contestantValue) {
+				|| highestDates[nextVertexName] < contestantValue) { // Si il n'y a pas encore de plus long chemin OU si le nouveau chemin est plus long
+			// On définit le plus long chemin comme étant le nouveau
 			highestDates[nextVertexName] = contestantValue;
 			maxOrigins[nextVertexName] = vertexName;
 		}
 
-		tabs = std::tie(highestDates, maxOrigins);
+		tabs = std::tie(highestDates, maxOrigins);  // On créé le nouveau tabs
 		return tabs;
 	};
 
 	tabs = std::tie(highestDates, maxOrigins);
-	tabs = fold(g, tabs, calcHighestDates);
+	tabs = fold(g, tabs, calcHighestDates); // On applique calcHighestDates à tous les éléments.
 	std::tie(highestDates, maxOrigins) = tabs;
 
+	// Impression du graphe (parcours à l'envers, depuis l'origine)
 	std::cout << std::endl
 	          << "digraph G {" << std::endl;
 
@@ -158,6 +172,6 @@ int main() {
 	}
 
 	std::cout << "}" << std::endl;
-
+	// Fin de l'impression du graphe
 	return 0;
 }
